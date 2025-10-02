11104f4266684fbdefd79f3fc0275a53d479cea3
cauanfaveri <cauanfaveri05@gmail.com>
2025-10-02 11:13:13 -0300

    Fim do Ex3

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ const app = express();
 const PORT = 8081;
 
 app.use(express.json())
-// app.post('/mensagem', (req, res) => {
-//     try {
-//         // Forma 1 - desestruturação
-//         const { id, descricao, valor } = req.body.dadosProdutos;
-//         const { nome, cpf } = req.body.dadosCliente;
 
-//         console.log(id, descricao, valor)
-//         console.log(nome, cpf)
-//     });
-
-app.post('/mensagem', (req, res) => {
+app.post('/login', (req, res) => {
     try {
-        const { nome, idade, time } = req.body;
-        console.log(nome, idade, time);
-        res.status(201).json({ message: `Ola ${nome}, Você tem ${idade} anos e torce para o ${time}!`});
-
+        const { usuario, senha} = req.body;
+        console.log(usuario, senha);
+        if (usuario === "admin" && senha === "1234"){
+            res.status(201).json({ message: `Dados corretos`});
+        } else {
+            res.status(201).json({ message: `Dados incorretos`});
+        }
     } catch (error) {
        console.error(error);
         res.status(500).json({ message: 'Ocorreu um erro ao processar', errorMessage: error.message}) 
