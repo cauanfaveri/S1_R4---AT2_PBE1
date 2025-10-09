7623629d7a9cd076511503e8cf4ae84aa1188d22
cauanfaveri <cauanfaveri05@gmail.com>
2025-10-09 08:36:10 -0300

    AJustando erro na validação

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const app = express();
 const PORT = 8081;
 
 app.use(express.json())
-async function validaçao(pNumUm, pNumDois) {
+async function validaçao(pNumUm, pNumDois, pNumTres) {
     try {
-        if (isNaN(pNumUm) || isNaN(pNumDois)) {
+        if (isNaN(pNumUm) || isNaN(pNumDois || isNaN(pNumTres))) {
             throw new Error("Os valores digitados são inválidos")
         }
-        const numero1 = parseFloat(pNumUm);
-        const numero2 = parseFloat(pNumDois);
-        const numero3 = parseFloat(pNumDois);
-        return { numero1, numero2, numero3};
+        const num1 = parseFloat(pNumUm);
+        const num2 = parseFloat(pNumDois);
+        const num3 = parseFloat(pNumTres);
+        return { num1, num2, num3};
     } catch (error) {
         throw new Error(error)
     }
 }
 
-app.post('/soma', (req, res) => {
+app.post('/soma', async (req, res) => {
     try {
         const { numero1, numero2, numero3 } = req.body;
         console.log(numero1, numero2, numero3);
-        let resultado;
-        resultado = numero1 + numero2 + numero3
+        const {num1, num2, num3} = await validaçao(numero1,  numero2, numero3);
+        resultado = num1 + num2 + num3
         console.log(resultado)
         res.status(201).json({ message: `${resultado}`});
 
